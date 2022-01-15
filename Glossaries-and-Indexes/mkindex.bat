6900b2126ae3c8600e56e3f095abc693aed0b62b
WilloughbySeago <47758790+WilloughbySeago@users.noreply.github.com>
2022-01-15 14:21:27 +0000

    Added quite flags

--- Glossaries-and-Indexes/mkindex.bat
+++ Glossaries-and-Indexes/mkindex.bat
@@ -3,6 +3,6 @@ ECHO OFF
 :: TITLE makeindex sets the title of the command line to makeindex
 TITLE makeindex
 :: Run the makeindex files for the index, glossary, and acronyms
-makeindex -s <path to indexstyle.ist> %1
-makeindex -s <path to glossarystyle.gst> -o %1.gls %1.glo
-makeindex -s <path to glossarystyle.gst> -o acronym.gls acronym.glo
+makeindex -q -s <path to indexstyle.ist> %1
+makeindex -q -s <path to glossarystyle.gst> -o %1.gls %1.glo
+makeindex -q -s <path to glossarystyle.gst> -o acronym.gls acronym.glo
